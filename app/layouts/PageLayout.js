d2549fc3f7c78e134509d346a5ea87c74e34dcc0
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-02-14 11:41:38 -0700

    Added page layout
    Added page layout to use for pages

---
+++ app/layouts/PageLayout.js
@@ -0,0 +1,15 @@
+"use client";
+
+import {motion} from "framer-motion";
+
+export default function PageLayout({children, className}){
+    return (
+      <motion.main className={`flex flex-col min-h-screen py-20 px-10 ${className}`}
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        transition={{duration: 0.4}}
+        >
+        {children}
+      </motion.main>
+    )
+  }
