df423005691c9c89fd8e09f3cfd46e9cdbab3f84
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-02-14 11:42:48 -0700

    Created profile page
    Initial creation of profile page

---
+++ app/(pages)/profile/page.js
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+    return (
+      <main className="flex flex-col justify-center items-center min-h-screen">
+        <p>Profile Page</p>
+        <p>Still in construction... </p>
+      </main>
+    );
+  }
+  
