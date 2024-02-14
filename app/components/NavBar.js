e629c7d6f68a60d59fce5b761a1e983c32fd792c
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-02-14 11:42:34 -0700

    Update in NavBar
    Added minor UI changes to navbar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -12,10 +12,13 @@ import {
 import Link from "next/link";
 
 import { useState } from "react";
+import { usePathname } from 'next/navigation'
 
 export default function NavBar() {
   // Used for menu
   const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  const pathname = usePathname();
 
   const handleOnMenuClick = () => {
     setIsMenuOpened(true);
@@ -28,19 +31,23 @@ export default function NavBar() {
   return (
     <header className="bg-gray-50 w-full h-20 fixed top-0 left-0 px-10 flex items-center sm:justify-between justify-end border shadow-sm">
       <div className="sm:flex flex-row gap-10 hidden">
-        <Link href="/" className="font-semibold text-2xl text-gray-600">
-          Home
+        <Link href="/" className={`font-semibold text-2xl ${pathname == "/" ? "text-indigo-700" : "text-gray-600"}`}>
+          Home 
         </Link>
-        <Link href="/cats" className="font-semibold text-2xl text-gray-600">
+        <Link href="/cats" className={`font-semibold text-2xl ${pathname == "/cats" ? "text-indigo-700" : "text-gray-600"}`}>
           Cats
         </Link>
-        <Link href="/dogs" className="font-semibold text-2xl text-gray-600">
+        <Link href="/dogs" className={`font-semibold text-2xl ${pathname == "/dogs" ? "text-indigo-700" : "text-gray-600"}`}>
           Dogs
         </Link>
       </div>
       <div className="sm:flex hidden flex-row gap-10">
-        <IoCartOutline size="30" />
-        <IoPersonOutline size="30" />
+        <Link href="/cart">
+          <IoCartOutline size="30" className={`font-semibold text-2xl ${pathname == "/cart" ? "text-indigo-700" : "text-gray-600"}`}/>
+        </Link>
+        <Link href="/profile">
+          <IoPersonOutline size="30" className={`font-semibold text-2xl ${pathname == "/profile" ? "text-indigo-700" : "text-gray-600"}`}/>
+        </Link>
       </div>
       <div className="sm:hidden block">
         {isMenuOpened ? (
