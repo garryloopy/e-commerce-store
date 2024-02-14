564f42658e5e90b3480426bd35213a43649114e0
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-02-14 11:43:31 -0700

    Update cats page

--- app/(pages)/cats/page.js
+++ app/(pages)/cats/page.js
@@ -2,6 +2,7 @@ export default function CatsPage() {
   return (
     <main className="flex flex-col justify-center items-center min-h-screen">
       <p>Cats Page</p>
+      <p>Still in construction... </p>
     </main>
   );
 }
