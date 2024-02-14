b7b2e03be1d4c76f6ef0c991485a40fe499af8a7
Garry Jr Dayag <garrydayagjr@gmail.com>
2024-02-14 11:42:16 -0700

    Added StoreItem
    Add a store item component. Used throughout the store to showcase an item.

--- app/components/StoreItem.js
+++ app/components/StoreItem.js
@@ -6,17 +6,33 @@ import Image from "next/image";
 
 import { IoHeart } from "react-icons/io5";
 
-const QuantityButton = ({ children, isSelected }) => {
+import { useState } from "react";
+
+const QuantityButton = ({ children, isSelected, onClick }) => {
+
+  const handleOnClick = () => {
+    if (onClick) {
+      onClick(children);
+    }
+  }
+
   return (
-    <button className="w-10 h-10 border rounded-md hover:bg-gray-100 hover:shadow-md active:bg-gray-50">
-      <p className="text-base font-semibold text-gray-700">{children}</p>
+    <button className={`w-10 h-10 border rounded-md ${isSelected ? "bg-indigo-600 hover:bg-indigo-700 border-0 shadow-sm active:bg-indigo-600" : ""} hover:bg-gray-100 hover:shadow-md active:bg-gray-50`}
+            onClick={handleOnClick}>
+      <p className={`text-base font-semibold ${isSelected ? "text-gray-50" : "text-gray-700"}`}>{children}</p>
     </button>
   );
 };
 
 export default function StoreItem() {
+  const [selectedQuantity, setSelectedQuantity] = useState(null);
+
+  const handleOnQuantityClick = (data) => {
+    setSelectedQuantity(data);
+  }
+
   return (
-    <div className="bg-gray-50 flex sm:flex-row flex-col w-full items-center rounded-md">
+    <div className="bg-gray-50 flex sm:flex-row flex-col sm:w-full w-fit items-center rounded-md border shadow-sm">
       <Image src={dog} width={500} height={500} alt="Dog" />
       <div className="p-8 w-full flex flex-col gap-6 justify-between">
         <div className="flex flex-col gap-8">
@@ -28,18 +44,18 @@ export default function StoreItem() {
           <div className="flex flex-col gap-4 border-b pb-4">
             <p className="text-lg font-semibold text-gray-700">Quantity:</p>
             <div className="flex flex-row gap-6">
-              <QuantityButton>XS</QuantityButton>
-              <QuantityButton>S</QuantityButton>
-              <QuantityButton>M</QuantityButton>
-              <QuantityButton>L</QuantityButton>
-              <QuantityButton>XL</QuantityButton>
-              <QuantityButton>XXL</QuantityButton>
+              <QuantityButton onClick={handleOnQuantityClick} isSelected={selectedQuantity == 1}>1</QuantityButton>
+              <QuantityButton onClick={handleOnQuantityClick} isSelected={selectedQuantity == 2}>2</QuantityButton>
+              <QuantityButton onClick={handleOnQuantityClick} isSelected={selectedQuantity == 3}>3</QuantityButton>
+              <QuantityButton onClick={handleOnQuantityClick} isSelected={selectedQuantity == 4}>4</QuantityButton>
+              <QuantityButton onClick={handleOnQuantityClick} isSelected={selectedQuantity == 5}>5</QuantityButton>
+              <QuantityButton onClick={handleOnQuantityClick} isSelected={selectedQuantity == 6}>6</QuantityButton>
             </div>
           </div>
         </div>
 
         <div className="flex flex-row justify-between">
-          <div className="flex flex-row sm:gap-8 gap-4">
+          <div className="flex flex-row gap-3">
             <button className="border w-36 h-12 rounded-md hover:bg-gray-100 hover:shadow-md active:bg-gray-50">
               <p className="text-lg font-medium text-gray-700">Buy now</p>
             </button>
@@ -48,7 +64,7 @@ export default function StoreItem() {
             </button>
           </div>
           <button className="border w-12 h-12 rounded-md text-center flex items-center justify-center hover:bg-gray-100 hover:shadow-md active:bg-gray-50">
-            <IoHeart size={25} className="text-gray-700" />
+            <IoHeart size={25} className="text-gray-700/50" />
           </button>
         </div>
       </div>
